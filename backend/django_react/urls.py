767add0317e017dc92b4f7dca46016208fa371f7
sandrorvr <sandrorvr@yahoo.com.br>
2023-01-05 20:08:02 -0300

    x

--- backend/django_react/urls.py
+++ backend/django_react/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'))
 ]
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))] 
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
--- backend/info_general/admin.py
+++ backend/info_general/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Servidor, Roteiro, Afinidade, Afastamento, Fds, Pedido
 
-# Register your models here.
+@admin.register(Servidor)
+class ServidorAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'nome', 'grupo', 'setor')
+
+@admin.register(Roteiro)
+class RoteiroAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'posto_base', 'roteiro', 'data_registro')
+
+@admin.register(Afinidade)
+class AfinidadeAdmin(admin.ModelAdmin):
+    list_display = ('data_registro', 'matricula', 'matricula_rx', 'cod_roteiro')
+
+@admin.register(Afastamento)
+class AfastamentoAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'motivo', 'data_inicio', 'data_fim', 'data_registro')
+
+@admin.register(Fds)
+class FdsAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'area', 'posto_base', 'equipamento', 'data', 'hora_entrada', 'hora_saida')
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'solicitacao', 'data_inicio', 'data_fim', 'participantes', 'data_registro')
