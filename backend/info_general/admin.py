93754242aa3b9611ca37e98b24415a138704ca9d
sandrorvr <sandrorvr@yahoo.com.br>
2023-01-12 22:07:26 -0300

    model_v2

--- backend/info_general/admin.py
+++ backend/info_general/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
-from .models import Servidor, Roteiro, Afastamento, Fds, Pedido
+from .models import Servidor, Local, Afastamento, Escala, Pedido, Guarnica
 
 @admin.register(Servidor)
 class ServidorAdmin(admin.ModelAdmin):
-    list_display = ('matricula', 'nome', 'grupo', 'setor', 'parceiro')
+    list_display = ('matricula', 'nome', 'grupo', 'setor', 'turno')
 
-@admin.register(Roteiro)
+@admin.register(Local)
 class RoteiroAdmin(admin.ModelAdmin):
-    list_display = ('cod_roteiro', 'posto_base', 'descricao', 'data_registro')
+    list_display = ('cod_local', 'posto_base', 'roteiro', 'area')
 
 
 @admin.register(Afastamento)
 class AfastamentoAdmin(admin.ModelAdmin):
-    list_display = ('matricula', 'motivo', 'data_inicio', 'data_fim', 'data_registro')
+    list_display = ('cod_afastamento', 'matricula', 'motivo', 'data_inicio', 'data_fim', 'data_registro')
 
-@admin.register(Fds)
+@admin.register(Escala)
 class FdsAdmin(admin.ModelAdmin):
-    list_display = ('matricula', 'area', 'posto_base', 'equipamento', 'data', 'hora_entrada', 'hora_saida')
+    list_display = ('cod_escala', 'cod_guarnica', 'cod_local', 'equipamento', 'data', 'hora_entrada', 'hora_saida')
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('matricula', 'solicitacao', 'data_inicio', 'data_fim', 'participantes', 'data_registro')
+
+
+@admin.register(Guarnica)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('cod_guarnicao', 'data', 'parceiro_tx', 'parceiro_rx')
--- backend/info_general/models.py
+++ backend/info_general/models.py
@@ -16,48 +16,72 @@ class Servidor(models.Model):
         ('gasup','GASUP'),
         ('geren', 'GERENCIA')
         ]
-    matricula = models.CharField(max_length=7)
+    
+    TUNO = [
+        ('n','NOTURNO'),
+        ('v','VESPERTINO'),
+        ('m','MATUTINO'),
+        ]
+    matricula = models.CharField(max_length=7, primary_key=True)
     nome = models.CharField(max_length=120)
     grupo = models.CharField(max_length=1, choices=GRUPOS, blank=False)
-    setor = models.CharField(max_length=25, choices=SETORES)
-    parceiro = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    setor = models.CharField(max_length=5, choices=SETORES)
+    turno = models.CharField(max_length=1, choices=TUNO, blank=False)
 
     def __str__(self):
         return self.nome
 
-class Area(models.Model):
-    cod_area = models.CharField(max_length=5, primary_key=True)
-    nome = models.CharField(max_length=60)
-    descricao = models.TextField(max_length=500)
-    data_registro = models.DateField(auto_now=True)
-
-class Roteiro(models.Model):
-    cod_roteiro = models.CharField(max_length=5, primary_key=True)
-    posto_base = models.CharField(max_length=120)
-    descricao = models.TextField(max_length=500)
-    data_registro = models.DateField(auto_now=True)
+class Local(models.Model):
+    AREA = [
+        ('a1','AREA 1'),
+        ('a2','AREA 2'),
+        ('a3','AREA 3'),
+        ('a4','AREA 4'),
+        ('a5','AREA 5'),
+        ('a6','AREA 6'),
+        ('a7','AREA 7'),
+        ('a8','AREA 8'),
+        ('a9','AREA 9'),
+        ('a10','AREA 10'),
+        ('a11','AREA 11'),
+        ]
+    cod_local = models.AutoField(primary_key=True)
+    posto_base = models.CharField(max_length=60)
+    roteiro = models.TextField(max_length=500)
+    area = models.CharField(max_length=3, choices=AREA, blank=False)
 
 
 class Afastamento(models.Model):
+    cod_afastamento = models.AutoField(primary_key=True)
     matricula = models.ForeignKey(Servidor, on_delete=models.CASCADE)
     motivo = models.TextField(max_length=500)
     data_inicio = models.DateField(blank=False)
     data_fim = models.DateField(blank=False)
     data_registro = models.DateField(auto_now=True)
 
-class Fds(models.Model):
+
+class Guarnica(models.Model):
+    cod_guarnicao = models.AutoField(primary_key=True)
+    data = models.DateField(null=False)
+    parceiro_rx = models.ForeignKey(Servidor, on_delete=models.CASCADE, related_name='parceiro_rx')
+    parceiro_tx = models.ForeignKey(Servidor, on_delete=models.CASCADE, related_name='parceiro_tx')
+
+class Escala(models.Model):
     VEICULO = [
         ('vtr', 'VIATURA'),
         ('moto', 'MOTO'),
         ('po', 'PO')
     ]
-    matricula = models.ForeignKey(Servidor, on_delete=models.CASCADE)
-    area = models.CharField(max_length=50, blank=False)
-    posto_base = models.CharField(max_length=80)
+    cod_escala = models.AutoField(primary_key=True)
+    cod_guarnica = models.ForeignKey(Guarnica, on_delete=models.CASCADE)
+    cod_local = models.ForeignKey(Local, on_delete=models.CASCADE)
     equipamento = models.CharField(max_length=5, choices=VEICULO)
     data = models.DateField()
     hora_entrada = models.TimeField()
-    hora_saida = models.TimeField()
+    hora_saida = models.TimeField() 
+
+
+
 
 class Pedido(models.Model):
     matricula = models.ForeignKey(Servidor, on_delete=models.CASCADE)
